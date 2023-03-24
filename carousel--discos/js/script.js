61cf3558aadf2762bb803c8399b00eb144b07148
lucianaoyhanarte <oyhanarteluciana@gmail.com>
2023-03-24 12:43:42 -0300

    va tomando forma

---
+++ carousel--discos/js/script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        center: true,
+        nav: true,
+        navtext:[
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-rigth'></i>",
+        ],
+        responsive:{
+            0:{
+                items:1,
+               
+            },
+            900:{
+                items:3,
+                
+            },
+            
+        }
+    })
+})
